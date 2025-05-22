f51efe98ae73c91743681a54ff3fca3a483eb5a9
amritchandra21 <140017372+amritchandra21@users.noreply.github.com>
2025-05-22 16:57:01 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (16.45%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -11,11 +11,15 @@ class Solution(object):
         :rtype: List[int]
         """
         res = []
-        def inorder(root):
-            if not root:
-                return
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-        inorder(root)
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        
         return res
