c2f7cbcc6fe950ee984349c42a1a06daa7be22df
amritchandra21 <140017372+amritchandra21@users.noreply.github.com>
2025-05-26 08:07:11 -0400

    Time: 111 ms (38.25%) | Memory: 19.5 MB (23.82%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        l, r = 0, 1
+        l = 0
         maxP = 0
-        while r < len(prices):
+        for r in range(1, len(prices)):
             if prices[l] > prices[r]:
                 l = r
-                r += 1
-            else:
-                diff = prices[r] - prices[l]
-                maxP = max(diff, maxP)
-                r += 1
+            maxP = max(maxP, prices[r] - prices[l])
         return maxP
